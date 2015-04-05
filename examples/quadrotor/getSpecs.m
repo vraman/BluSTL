c2d1ea56deb4b832e8ff71412e923356e402261d
Vasu Raman <vraman@cs.cornell.edu>
2015-04-04 18:41:12 -0700

    getSpecs automatically from env using mat2str

--- examples/quadrotor/getSpecs.m
+++ examples/quadrotor/getSpecs.m
@@ -1,13 +1,15 @@
-function [stl_list] = getSpecs( inits, dim, ex)
+function QS = getSpecs(QS)
 
 %% Create the environment
+ex = 6;
+L = QS.L;
 
 env = envLTL(ex);
 
 % POLYTOPE CONSTRAINTS
 % Control constraints
-inits.var.Hu = [eye(3); -eye(3)];
-inits.var.Ku = 0.04*[9.935; 3.62; 3.62;  
+Hu = [eye(3); -eye(3)];
+Ku = 0.04*[9.935; 3.62; 3.62;  
            4.545; 3.62; 3.62];
       
 % State constraints
@@ -15,71 +17,62 @@ p = [0, 10]; % m
 v = Inf*[-1, 1]; % m/s
 r = Inf*[-1, 1]; % rad
 w = Inf*[-1; 1]; % rad/s
-inits.var.Hx = [eye(10); -eye(10)];
-inits.var.Kx = [ p(2)*ones(3, 1);  v(2)*ones(3, 1);  r(2)*ones(2, 1);  w(2)*ones(2, 1);
+Hx = [eye(10); -eye(10)];
+Kx = [ p(2)*ones(3, 1);  v(2)*ones(3, 1);  r(2)*ones(2, 1);  w(2)*ones(2, 1);
       -p(1)*ones(3, 1); -v(1)*ones(3, 1); -r(1)*ones(2, 1); -w(1)*ones(2, 1)];
 
   
-%fPoly = {'alw_[0,Inf] ((var.Hx*X(:,t) <= var.Kx))','alw_[0,Inf] ((var.Hu*U(:,t-1) <= var.Ku))'};
-%fPoly = {'alw_[0,Inf] ((var.Hx*X(:,t) <= var.Kx))'};
+fPoly = {strcat('alw_[0,Inf] (',mat2str(Hx),'*X(:,t) <= ',mat2str(Kx),')'), ...
+         strcat('alw_[0,Inf] (',mat2str(Hu),'*U(:,t-1) <= ',mat2str(Ku),')')};
 fPoly = {};
 
-% SAFETY
-if 3 == dim
-    inits.var.C = [1 zeros(1, 9); 
-             0 1 zeros(1, 8);
-             0 0 1 zeros(1, 7)];
-elseif 2 == dim
-    inits.var.C = [1 zeros(1, 9); 
-             0 1 zeros(1, 8)];
-end
 
-G = env.work.unsafe;
+G = env.work.safe;
 fSafe = {};
 if ~isempty(G)
     % Create safety constraints -- disjunction of polytopes
     nPoly = length(G);
-    inits.var.HG = cell(1, nPoly);
-    inits.var.KG = cell(1, nPoly);
+    fSafe = strcat('alw_[0, Inf] (');
     for i = 1:nPoly
-        [h, l] = double(G(i));
-        inits.var.HG{i} = h;
-        inits.var.KG{i} = l;
-        fSafe = [fSafe, sprintf('alw_[0,Inf](var.HG{%d}*var.C*X(:,t) > var.KG{%d})',i,i)];
+            [h,l] = double(G(i));
+            if i~=1
+                fSafe = [fSafe, ' or ('];
+            end
+            fSafe = [fSafe, strcat(mat2str(h),'*Y(:,t) <= ',mat2str(l))];
+    end
+    for j = 1:nPoly
+        fSafe = [fSafe, ')'];
     end
 else
     fSafe = {};
 end
 fSafe = {};
 
+
 if ex==3
     F =  {env.AP.a, env.AP.b, env.AP.c};
+elseif ex==6
+	F = {env.AP.a, env.AP.b};
 else
-    F =  {env.AP.a, env.AP.b, env.AP.c, env.AP.d, env.AP.e};
+	F =  {env.AP.a, env.AP.b, env.AP.c, env.AP.d, env.AP.e};
 end
 
 
 if ~isempty(F)
     % Create "goal" constraints
     fGoal = {};
-    inits.var.HF = cell(1,length(F));
-    inits.var.KF = cell(1,length(F));
     
     for i = 1:length(F)
         nPoly = length(F{i});   
-
-        inits.var.HF{i} = cell(1, nPoly);
-        inits.var.KF{i} = cell(1, nPoly);
         
-        fGoal{i} = '(ev_[0,Inf] (';
+        fGoal{i} = strcat('alw_[0, Inf] (ev_[0,',int2str(L),'] (');
         for j = 1:nPoly
             [h,k] = double(F{i}(j));
-            inits.var.HF{i}{j} = h;
-            inits.var.KF{i}{j} = k;
+            
             if j~=1
                 fGoal{i} = [fGoal{i}, ' or ('];
             end
-            fGoal{i} = [fGoal{i}, sprintf('(var.HF{%d}{%d}*var.C*X(:,t) <= var.KF{%d}{%d})',i,j,i,j)];
+            fGoal{i} = [fGoal{i}, strcat(mat2str(h),'*Y(:,t) <= ',mat2str(l))];
            
         end
         for j = 1:nPoly-1
@@ -90,10 +83,10 @@ if ~isempty(F)
 else
     fGoal = {};
 end
-%fGoal={};
+fGoal={};
 
 %% STL formula
-stl_list = [fPoly, fSafe, fGoal{:}];
+QS.stl_list = [QS.stl_list, fPoly, fSafe, fGoal{:}];
 
 end
 
