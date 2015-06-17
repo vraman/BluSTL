75256e03e233e2159aa27407fb7dd97ac7cf6219
Vasu Raman <vraman@cs.cornell.edu>
2015-06-16 17:22:47 -0700

    introduced list of indices optimization for boolean and robust (non-interval) encodings

--- src/STL2MILP_robust.m
+++ src/STL2MILP_robust.m
@@ -1,4 +1,4 @@
-function [F,P] = STL2MILP_robust(phi,k,ts,var,M)
+function [F,P] = STL2MILP_robust(phi,kList,kMax,ts,var,M)
 % STL2MILP_robust  constructs MILP constraints in YALMIP that compute
 %                  the robustness of satisfaction for specification phi
 %
@@ -33,74 +33,73 @@ function [F,P] = STL2MILP_robust(phi,k,ts,var,M)
     
     a = interval(1);
     b = interval(2);
-    
-    if a == Inf
-        a = k*ts;
-    end
-    if b == Inf
-        b = k*ts;
-    end
-    
+        
     a = max([0 floor(a/ts)-1]); 
     b = ceil(b/ts)-1; 
+    
+    if b==Inf
+        b = kMax;
+    end
     
     switch (phi.type)
         
         case 'predicate'
-            [F,P] = pred(phi.st,k,var,M);
+            [F,P] = pred(phi.st,kList,kMax,var,M);
                      
         case 'not'
-            [Frest,Prest] = STL2MILP_robust(phi.phi,k,ts, var,M);
+            [Frest,Prest] = STL2MILP_robust(phi.phi,kList,kMax,ts, var,M);
             [Fnot, Pnot] = not(Prest);
             F = [F, Fnot, Frest];
             P = Pnot;
 
         case 'or'
-            [Fdis1,Pdis1] = STL2MILP_robust(phi.phi1,k,ts, var,M);
-            [Fdis2,Pdis2] = STL2MILP_robust(phi.phi2,k,ts, var,M);
+            [Fdis1,Pdis1] = STL2MILP_robust(phi.phi1,kList,kMax,ts, var,M);
+            [Fdis2,Pdis2] = STL2MILP_robust(phi.phi2,kList,kMax,ts, var,M);
             [For, Por] = or([Pdis1;Pdis2],M);
             F = [F, For, Fdis1, Fdis2];
             P = Por;
 
         case 'and'
-            [Fcon1,Pcon1] = STL2MILP_robust(phi.phi1,k,ts, var,M);
-            [Fcon2,Pcon2] = STL2MILP_robust(phi.phi2,k,ts, var,M);
+            [Fcon1,Pcon1] = STL2MILP_robust(phi.phi1,kList,kMax,ts, var,M);
+            [Fcon2,Pcon2] = STL2MILP_robust(phi.phi2,kList,kMax,ts, var,M);
             [Fand, Pand] = and([Pcon1;Pcon2],M);
             F = [F, Fand, Fcon1, Fcon2];
             P = Pand;
 
         case '=>'
-            [Fant,Pant] = STL2MILP_robust(phi.phi1,k, ts,var,M);
-            [Fcons,Pcons] = STL2MILP_robust(phi.phi2,k,ts, var,M);
+            [Fant,Pant] = STL2MILP_robust(phi.phi1,kList,kMax,ts,var,M);
+            [Fcons,Pcons] = STL2MILP_robust(phi.phi2,kList,kMax,ts, var,M);
             [Fnotant,Pnotant] = not(Pant);
             [Fimp, Pimp] = or([Pnotant;Pcons],M);
             F = [F, Fant, Fnotant, Fcons, Fimp];
             P = [Pimp,P];
             
         case 'always'
-            [Frest,Prest] = STL2MILP_robust(phi.phi,k, ts, var,M);
-            [Falw, Palw] = always(Prest,a,b,k,M);
+            kListAlw = unique(cell2mat(arrayfun(@(k) {k + a: k + b}, kList)));
+            [Frest,Prest] = STL2MILP_robust(phi.phi,kListAlw,kMax,ts, var,M);
+            [Falw, Palw] = always(Prest,a,b,kList,kMax,M);
             F = [F, Falw];
             P = [Palw, P];
             F = [F, Frest];
 
         case 'eventually'
-            [Frest,Prest] = STL2MILP_robust(phi.phi,k, ts, var,M);
-            [Fev, Pev] = eventually(Prest,a,b,k,M);
+            kListEv = unique(cell2mat(arrayfun(@(k) {k + a: k + b}, kList)));
+            [Frest,Prest] = STL2MILP_robust(phi.phi,kListEv,kMax,ts, var,M);
+            [Fev, Pev] = eventually(Prest,a,b,kList,kMax,M);
             F = [F, Fev];
             P = [Pev, P];
             F = [F, Frest];
           
         case 'until'
-            [Fp,Pp] = STL2MILP_robust(phi.phi1,k, ts, var,M);
-            [Fq,Pq] = STL2MILP_robust(phi.phi2,k, ts, var,M);
-            [Funtil, Puntil] = until(Pp,Pq,a,b,k,M);
+            [Fp,Pp] = STL2MILP_robust(phi.phi1,kList,kMax,ts, var,M);
+            [Fq,Pq] = STL2MILP_robust(phi.phi2,kList,kMax,ts, var,M);
+            [Funtil, Puntil] = until(Pp,Pq,a,b,kList,kMax,M);
             F = [F, Funtil, Fp, Fq];
             P = Puntil;
     end
 end
 
-function [F,z] = pred(st,k,var,M)
+function [F,z] = pred(st,kList,kMax,var,M)
     % Enforce constraints based on predicates 
     % 
     % var is the variable dictionary    
@@ -122,11 +121,13 @@ function [F,z] = pred(st,k,var,M)
     end
          
     F = [];
-    
     z = [];
+    
+    k = size(kList,2);
+    
     for l=1:k
         t_st = st;
-        t_st = regexprep(t_st,'\<t\>',num2str(l));
+        t_st = regexprep(t_st,'\<t\>',num2str(kList(l)));
         try 
             zl = eval(t_st);
         end
@@ -164,26 +165,34 @@ end
 
 % TEMPORAL OPERATIONS
 
-function [F,P_alw] = always(P, a,b, k,M)
+function [F,P_alw] = always(P, a,b,kList,kMax,M)
     F = [];
+    k = size(kList,2);
     P_alw = sdpvar(1,k);
+    kListAlw = unique(cell2mat(arrayfun(@(k) {k + a : k + b}, kList)));
     
     for i = 1:k
-        [ia, ib] = getIndices(i,a,b,k);
-        [F0,P0] = and(P(ia:ib)',M);
+        [ia, ib] = getIndices(kList(i),a,b,kMax);
+        ia_real = find(kListAlw==ia);
+        ib_real = find(kListAlw==ib);
+        [F0,P0] = and(P(ia_real:ib_real)',M);
         F = [F;F0,P_alw(i)==P0];
     end
     
 end
 
 
-function [F,P_ev] = eventually(P, a,b, k,M)
+function [F,P_ev] = eventually(P, a,b,kList,kMax,M)
     F = [];
+    k = size(kList,2);
     P_ev = sdpvar(1,k);
+    kListEv = unique(cell2mat(arrayfun(@(k) {k + a : k + b}, kList)));
     
     for i = 1:k
-        [ia, ib] = getIndices(i,a,b,k);
-        [F0,P0] = or(P(ia:ib)',M);
+        [ia, ib] = getIndices(i,a,b,kMax);
+        ia_real = find(kListEv==ia);
+        ib_real = find(kListEv==ib);
+        [F0,P0] = or(P(ia_real:ib_real)',M);
         F = [F;F0,P_ev(i)==P0];
     end
     
@@ -196,7 +205,7 @@ function [F,P_until] = until(Pp,Pq,a,b,k,M)
     P_until = sdpvar(1,k);
     
     for i = 1:k
-        [ia, ib] = getIndices(i,a,b,k);
+        [ia, ib] = getIndices(i,a,b,kMax);
         F0 = []; 
         P0 = [];
         for j = ia:ib
