80c4b42677538f0f650437f02c939fa86b057d8d
Vasu Raman <vraman@cs.cornell.edu>
2016-01-10 17:15:23 -0800

    cleanup of signal generator example

--- examples/signal_generator/test_encodings.m
+++ examples/signal_generator/test_encodings.m
@@ -10,13 +10,13 @@ SG = signal_generator(nu,nw);
 % Time
 SG.time = 0:1:100; % time for the dynamics
 SG.ts=1; % sampling time for controller
-SG.L=7;  % horizon (# of steps)
+SG.L=4;  % horizon (# of steps)
 SG.nb_stages=1; % repeats time
 
 % Bounds
 SG.u_ub(:) = 10;
 SG.u_lb(:) = -10;
-SG.lambda_rho = 0.1;
+SG.lambda_rho = 100;
 
 SG.Wref = [ cos(SG.time) ; 0.0*cos(SG.time)+1.1;];
 
@@ -25,20 +25,21 @@ phis= { '(w1(t)>0) or (w2(t)>0)', ...
     '((w1(t)>0)) or (ev_[0,1.1] (w1(t)>0))',...    
     'alw_[0,5] (w1(t)>0 )',...
     'not (alw_[0,5] (w1(t)>0))',...
-    'alw_[0,1.1] alw_[0,1.1] (w1(t)>0)',...
-    'alw( (x1(t)<1  =>  ev_[0, 6] (x1(t)>3)) and ((x1(t)>3)  =>  ev_[0, 6] (x1(t)<1)) )';
+    'alw_[0,1.1] (w1(t)>0)',...
+    'alw( (y1(t)<1  =>  ev_[0, 2] (y1(t)>3)) and ((y1(t)>3)  =>  ev_[0, 2] (y1(t)<2)) )',...
+    'ev_[0,10] (y1(t) > 3)';    
     }; 
 
 
-for i = 7
+for i = 2
     phi = phis{i};
     
     tic;
-    rob= monitor(SG, phi)
+    rob = monitor(SG, phi, 'robust');
     toc;
     
     tic;
-    rob_int = monitor_interval(SG, phi)
+    rob = monitor(SG, phi, 'interval');
     toc;
-
+    
 end
--- examples/signal_generator/test_signal_generator.m
+++ examples/signal_generator/test_signal_generator.m
@@ -1,54 +1,64 @@
 %% Create the system
-nu = 2;
-nw = 2;
+nu = 20;
+nw = 20;
 SG = signal_generator(nu,nw);
+SG = init_control(SG);
 
 %% Controller Initialisation
 % Time
-SG.time = 0:.1:100; % time for the dynamics
+SG.time = 0:.1:10; % time for the dynamics
 SG.ts=1; % sampling time for controller
-SG.L=7;  % horizon (# of steps)
+SG.L=3;  % horizon (# of steps)
 SG.nb_stages=1; % repeats time
 
 % Bounds
 SG.u_ub(:) = 10;
 SG.u_lb(:) = -10;
-SG.lambda_rho = 0.1;
+SG.lambda_rho = 1000;
+SG.lambda_t1 = 100;
 
-SG.Wref = [sin(SG.time); cos(SG.time)];
-%% STL formula
-% SG.stl_list = {'ev_[0, 13] (x1(t) > 0.1)'};
-% SG.stl_list = {'ev_[3,5] (y1(t) > 5 and y2(t)<-2)'};
 
-%% TODO: make it so the following would work:
-% SG.stl_list = {'ev_[3,5] ([y1(t);-y2(t)] > [5;2])'};
-% SG.stl_list = {'ev_[3,5] (Y(1:2,t) > 2)'};
+SG.Wref = kron([sin(SG.time); cos(SG.time)],ones(10,1));
 
-%SG.stl_list{1} = 'ev_[0, 6] (y1(t)>3)';
-SG.stl_list{1} = 'alw ( y1(t)<1 => ev_[0, 10] (y1(t)>3)) and ((y1(t)>3)  =>  ev_[0, 10] (y1(t)<1))';
-%SG.stl_list{1} = 'alw (y1(t)<1 => ev_[0, 10] (y1(t)>3) )';
-% SG.stl_list{1} = 'alw_[0,Inf] ( y1(t)<11 and ev_[0,5](y1(t)>0) and ev_[0,5](y1(t)<1))';
 
 SG.min_rob = 0.01;    
 SG.bigM = 1000;
 
-SG.plot_u = [1];
-SG.plot_y =[1];
-SG.plot_w = [1];
+SG.plot_u = [1, 2];
+SG.plot_y =[1,2];
+SG.plot_w = [1, 2];
 
-%% Initial state
+%% STL formula
+SG.stl_list{1} = '((y1(t)<0) => ev_[0, 5] (y1(t)>5)) and ((y1(t)>5) =>  ev_[0, 5](y1(t)<0)) and ((y2(t)<0) => ev_[0, 5] (y2(t)>5)) and ((y2(t)>5) =>  ev_[0, 5](y2(t)<0))';
 
-%return;
+
 %% running stuff
+
+% MONITORING
+% for i=1:numel(SG.stl_list)
+%     rob = monitor(SG, SG.stl_list{i})
+% end
+
+% DETERMINISTIC
 fprintf('Computing controller...');
 tic;
 SG.controller = get_controller(SG,'robust');
 toc;
 SG = SG.reset_data();
+SG = SG.run_deterministic(SG.controller);
 
-[SG, status] = compute_input(SG, SG.controller);
-if status==0
-SG = apply_input(SG);
-SG = update_plot(SG);
-end;
+% ADVERSARIAL
+SG.w_ub(:) = 3;
+SG.w_lb(:) = -3;
+SG.is_det = 0;
 
+fprintf('Computing controller...');
+tic;
+SG.controller = get_controller(SG,'robust');
+toc;
+fprintf('Computing adversary...');
+tic;
+SG.adversary = get_adversary(SG);
+toc;
+SG = SG.reset_data();
+SG = SG.run_adversarial(SG.controller,SG.adversary);
